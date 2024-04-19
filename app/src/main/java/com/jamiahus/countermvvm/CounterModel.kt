8cc120c45b9aed5427d99cdb19119e1c826f3355
jamiahuswalton <jamiahuswalton@gmail.com>
2024-04-18 23:16:53 -0500

    Initial commit

---
+++ app/src/main/java/com/jamiahus/countermvvm/CounterModel.kt
@@ -0,0 +1,18 @@
+package com.jamiahus.countermvvm
+
+data class CounterModel (var count: Int){
+}
+
+class CounterRepository{
+    private var _counter = CounterModel(0)
+
+    fun getcounter() = _counter
+
+    fun incrementCounter(){
+        _counter.count++
+    }
+
+    fun decrementCounter(){
+        _counter.count--
+    }
+}
